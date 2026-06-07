a9ba0a19b7dbde5f01df2aa96ff866d87289e614
Summer2077 <summer042055@gmail.com>
2026-06-08 01:17:14 +0800

    polish: verify widget shell interactions

--- Sources/CodexIslandUsageWidget/RootWidgetView.swift
+++ Sources/CodexIslandUsageWidget/RootWidgetView.swift
@@ -25,5 +25,17 @@ struct RootWidgetView: View {
         .frame(width: viewModel.isExpanded ? 292 : 118, alignment: .top)
         .animation(.spring(response: 0.24, dampingFraction: 0.88), value: viewModel.isExpanded)
         .animation(.spring(response: 0.24, dampingFraction: 0.88), value: viewModel.settings.showsWeeklyQuotaInDefault)
+        .preferredColorScheme(preferredColorScheme)
+    }
+
+    private var preferredColorScheme: ColorScheme? {
+        switch viewModel.settings.appearanceMode {
+        case .system:
+            return nil
+        case .dark:
+            return .dark
+        case .light:
+            return .light
+        }
     }
 }
